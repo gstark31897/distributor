576f7c33fb1e27f5394ae6d40c6638f954a9cc46
Gavin Stark <gstark31897@gmail.com>
2017-06-14 23:34:56 -0500

    making tidy actually do the thing

--- main.pl
+++ main.pl
@@ -6,33 +6,43 @@ use strict;
 
 my $client = MongoDB->connect('mongodb://localhost');
 my $keyval = $client->ns('distributor.keyval');
-my $meta = $client->ns('distributor.meta');
+my $meta   = $client->ns('distributor.meta');
 
 get '/data/:key' => sub {
-    my $c = shift;
-    my $key = $c->param('key');
-    my $data = $keyval->find_one({ key => "$key" });
+    my $c    = shift;
+    my $key  = $c->param('key');
+    my $data = $keyval->find_one( { key => "$key" } );
     print "$data\n";
-    $c->render(json => $data)
+    $c->render( json => $data );
 };
 
 post '/data/:key' => sub {
-    my $c = shift;
-    my $key = $c->param('key');
+    my $c     = shift;
+    my $key   = $c->param('key');
     my $value = $c->req->json->{'value'};
-    my $hash = sha3_256_hex($value);
-    my $data = $keyval->find_one({ key => "$key" });
+    my $hash  = sha3_256_hex($value);
+    my $data  = $keyval->find_one( { key => "$key" } );
     if ($data) {
-        $keyval->update_many({ key => "$key" }, { '$set' => {value => "$value", hash => "$hash"}});
-    } else {
-        my $result = $keyval->insert_one({ key => "$key", value => "$value", hash => "$hash" });
+        $keyval->update_many( { key => "$key" },
+            { '$set' => { value => "$value", hash => "$hash" } } );
     }
-    $c->render(json => {hash => "$hash"})
+    else {
+        my $result = $keyval->insert_one(
+            { key => "$key", value => "$value", hash => "$hash" } );
+    }
+    $c->render( json => { hash => "$hash" } );
 };
 
 get '/meta/range' => sub {
     my $c = shift;
-    $c->render(json => {min => '0000000000000000000000000000000000000000000000000000000000000000', max => 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'});
+    $c->render(
+        json => {
+            min =>
+'0000000000000000000000000000000000000000000000000000000000000000',
+            max =>
+              'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+        }
+    );
 };
 
 app->start;
