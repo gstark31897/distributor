7aca01ffb02f2d48a79673b2ced577f961984f04
Gavin Stark <gstark31897@gmail.com>
2017-06-26 23:41:00 -0500

    adding some meta data

--- main.pl
+++ main.pl
@@ -16,6 +16,49 @@ my $client = MongoDB->connect('mongodb://localhost');
 my $keyval = $client->ns('distributor.keyval');
 my $meta   = $client->ns('distributor.meta');
 
+sub find_min_max {
+    # TODO test without any data
+    my $max_cur = $keyval->find()->sort( { 'hash' => -1 } )->limit(1);
+    my $min_cur = $keyval->find()->sort( { 'hash' => 1 } )->limit(1);
+    my $max     = "";
+    my $min     = "";
+
+    while ( my $item = $max_cur->next ) {
+        $max = $item->{'hash'};
+    }
+    while ( my $item = $min_cur->next ) {
+        $min = $item->{'hash'};
+    }
+
+    update_meta( 'min', $min );
+    update_meta( 'max', $max );
+}
+
+sub update_meta {
+    my $key   = shift;
+    my $value = shift;
+    my $item  = $meta->find_one( { key => "$key" } );
+    if ($item) {
+        $meta->update_many( { key => "$key" },
+            { '$set' => { value => "$value" } } );
+    }
+    else {
+        $meta->insert_one( { key => "$key", value => "$value" } );
+    }
+}
+
+sub get_meta {
+    my $key = shift;
+    my $cur = $meta->find( { key => "$key" } );
+    while ( my $item = $cur->next ) {
+        my $value = $item->{'value'};
+        print "value: $value\n";
+        return $value;
+    }
+}
+
+find_min_max();
+
 get '/data/:key' => sub {
     my $c    = shift;
     my $key  = $c->param('key');
@@ -53,13 +96,13 @@ post '/meta' => sub {
     # TODO store these values
     # TODO make a daemon that sends these requests out randomly
     # TODO make the min and max actually work
+    my $my_min = get_meta('min');
+    my $my_max = get_meta('max');
     $c->render(
         json => {
-            port => $port,
-            min =>
-'0000000000000000000000000000000000000000000000000000000000000000',
-            max =>
-'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+            port  => $port,
+            min   => "$my_min",
+            max   => "$my_max",
             count => 0,
             limit => 100
         }
